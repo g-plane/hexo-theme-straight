438371e1009e95a1d143246b0f45b8d124d58afb
Pig Fang <g-plane@hotmail.com>
2020-09-01 22:56:12 +0800

    timeago.js -> dayjs

--- components/TimeAgo.vue
+++ components/TimeAgo.vue
@@ -4,17 +4,22 @@
     :title="lastUpdated | formatDate"
     pubdate="pubdate"
   >
-    {{ lastUpdated | timeago($lang) }}
+    {{ lastUpdated | diff }}
   </time>
 </template>
 
 <script>
-import { format } from 'timeago.js'
+import dayjs from 'dayjs'
+import * as relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn'
+
+dayjs.extend(relativeTime)
+dayjs.locale('zh-cn')
 
 export default {
   name: 'TimeAgo',
   filters: {
-    timeago: str => format(new Date(str), 'zh_CN'),
+    diff: date => dayjs(date).toNow(),
     formatDate: date => new Date(date).toLocaleDateString(),
   },
   props: {
