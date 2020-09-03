2a58ef8752318fc465057b38045e723e6ecb0b0a
Pig Fang <g-plane@hotmail.com>
2020-09-03 10:37:23 +0800

    fix retrieving frontmatter

--- layouts/Post.vue
+++ layouts/Post.vue
@@ -6,7 +6,7 @@
       </h1>
       <time
         class="post-info"
-        datetime="$page.frontmatter.date"
+        datetime="$frontmatter.date"
         pubdate="pubdate"
       >{{ page.frontmatter.date.toLocaleDateString() }}</time>
       <span class="post-info post-tag">
